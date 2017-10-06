2be7be72f32d48a4d0de9d53833e249da548ef9a
Poonam Sagar <poonamsagar9212@gmail.com>
2017-10-06 16:01:05 +0530

    initial file

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+
+function add(a,b){
+    return a+b;
+}
+var d=add(5,6); //value of c
+console.log(d);
+
+
+function multiply(a,b,c){
+   return a*b*c;
+}
+var e=multiply(2,5,6);
+console.log(e);
+//multiply(5,2,2);
