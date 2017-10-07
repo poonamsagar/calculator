e70b7cdc99874c6c430f4a5ab9e1c795d67daad1
Poonam Sagar <poonamsagar9212@gmail.com>
2017-10-07 18:33:55 +0530

    Changes in Calculator

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,30 +1,42 @@
 
-function add(a,b){
-    alert("called from add");
-    return a+b;
+
+
+function add(){
+    var a=document.getElementById("firstValue").value;
+    var b=document.getElementById("secondValue").value;
+    var result=document.getElementById("result");
+    result.value=Number(a)+Number(b);
 }
 // var d=add(5,6); //value of c
 // console.log(d);
 
 
-function multiply(a,b,c){
-    alert("called from mult");
-   return a*b*c;
+function multiply(){
+    var a=document.getElementById("firstValue").value;
+    var b=document.getElementById("secondValue").value;
+    var result=document.getElementById("result");
+    result.value=Number(a)-Number(b);
 }
 // var e=multiply(2,5,6);
 // console.log(e);
 //multiply(5,2,2);
 
-function divide(a,b){
-    alert("called from divide");
-    return a/b;
+function divide(){
+   
+    var a=document.getElementById("firstValue").value;
+    var b=document.getElementById("secondValue").value;
+    var result=document.getElementById("result");
+    result.value=Number(a)*Number(b);
 }
 // var f=division(10,5);
 // console.log(f);
 
-function subtract(a,b){
-    alert("called from sub");
-    return a-b;
+function subtract(){
+    
+    var a=document.getElementById("firstValue").value;
+    var b=document.getElementById("secondValue").value;
+    var result=document.getElementById("result");
+    result.value=Number(a)/Number(b);
 }
 // var g=subtract(6,2);
 // console.log(g);
