0184ff6b019905f551ee8ef8f294c6ef0ecc80ef
Poonam Sagar <poonamsagar9212@gmail.com>
2017-10-07 18:06:02 +0530

    calculator logic

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,14 +1,30 @@
 
 function add(a,b){
+    alert("called from add");
     return a+b;
 }
-var d=add(5,6); //value of c
-console.log(d);
+// var d=add(5,6); //value of c
+// console.log(d);
 
 
 function multiply(a,b,c){
+    alert("called from mult");
    return a*b*c;
 }
-var e=multiply(2,5,6);
-console.log(e);
+// var e=multiply(2,5,6);
+// console.log(e);
 //multiply(5,2,2);
+
+function divide(a,b){
+    alert("called from divide");
+    return a/b;
+}
+// var f=division(10,5);
+// console.log(f);
+
+function subtract(a,b){
+    alert("called from sub");
+    return a-b;
+}
+// var g=subtract(6,2);
+// console.log(g);
